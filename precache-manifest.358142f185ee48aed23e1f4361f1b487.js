d33f6dff0477ef706dcf3f700443da3286c99ae4
jennifrmarie <jen@EPICACXIV.attlocal.net>
2020-04-12 14:07:55 -0400

    Updates

---
+++ precache-manifest.358142f185ee48aed23e1f4361f1b487.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06210f79923aca9d434d8049f8e37d67",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "1e70a3e891de462ae386",
+    "url": "/Portfolio/static/css/main.1bd35226.chunk.css"
+  },
+  {
+    "revision": "c3375bb2d41c95fe88e5",
+    "url": "/Portfolio/static/js/2.5c3d2ef8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Portfolio/static/js/2.5c3d2ef8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e70a3e891de462ae386",
+    "url": "/Portfolio/static/js/main.f16d5692.chunk.js"
+  },
+  {
+    "revision": "abb75eb31b030f7a3844",
+    "url": "/Portfolio/static/js/runtime-main.27665315.js"
+  }
+]);
